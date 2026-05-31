ecf815b95a9a50b7c5169f477ed5d18ba274ba18
Dagim <158840047+raodenn@users.noreply.github.com>
2026-05-31 13:42:02 +0300

    Configure static file storage with Whitenoise
    Added storage configuration for static files using Whitenoise.

--- internship_project/settings.py
+++ internship_project/settings.py
@@ -221,6 +221,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
