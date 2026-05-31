84b2f3576a6f579462bf3c7e23fb4d995673e2f5
Dagim <158840047+raodenn@users.noreply.github.com>
2026-05-31 13:36:26 +0300

    Add WhiteNoise middleware for static file handling

--- internship_project/settings.py
+++ internship_project/settings.py
@@ -106,6 +106,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
